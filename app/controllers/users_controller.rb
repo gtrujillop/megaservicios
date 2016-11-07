bd4c43a945cd89d6b39cdc95e6e24ed9760639c9
Gaston Trujillo <gaston.trujillo.java@gmail.com>
2016-11-07 16:33:39 -0500

    Devise: Add views setup and structure for devise views.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,46 @@
+class UsersController < ApplicationController
+  def show
+    @user = current_user
+  end
+
+  def user_details
+    @user = User.where(id: params[:user_id])
+                .includes(:appliances)
+                .first
+
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def index
+    @users = User.all
+    if @users.empty?
+      flash[:alert] = 'No hay clientes registrados.'
+    end
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Nuevo cliente registrado."
+      redirect_to user_list_path
+    else
+      flash[:error] = @user.errors.full_messages.join(',')
+      render 'new'
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:id,
+                                 :first_name,
+                                 :last_name,
+                                 :email,
+                                 :password,
+                                 :birth_date,
+                                 :address,
+                                 :phone)
+  end
+  private :user_params
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
+  root 'users#show'
+
+  scope :users, controller: 'users' do
+    post 'create', as: 'create_user'
+    get 'new', as: 'new_user'
+    get 'edit', as: 'edit_user'
+    put 'update', as: 'update_user'
+    get 'index', as: 'user_list'
+    delete 'destroy', as: 'destroy_user'
+    get 'user_details', as: 'user_details'
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
