c44e98228672597afbce52e04e2485c80080e38a
Gaston Trujillo <gaston.trujillo.java@gmail.com>
2016-11-07 16:34:14 -0500

    User: Add helper methods for views.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def applicances_count
+    appliances.count
+  end
+
+  def full_name
+    first_name + ' ' + last_name
+  end
 end
