98700112f79ba86a0f1086f6a637c3f6da4020df
Carlos Jaramillo <cejaramillof@gmail.com>
2017-04-10 23:06:51 -0500

    Fix: notificaciones snake and language

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -10,7 +10,7 @@ class ContactsController < ApplicationController
       render 'dashb'
     else
       @contacts = Contact.in_draft.latest
-      $totalrevised = @contacts.size
+      $total_revised = @contacts.size
       render 'index'      
     end
   end
