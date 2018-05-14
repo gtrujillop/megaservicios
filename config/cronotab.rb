9f459cffa1760a1104000b565c19bc395bcbe533
Gaston Trujillo <gaston.trujillo.java@gmail.com>
2018-05-14 11:15:36 -0500

    task-03: Configure mailer for local and production

--- config/cronotab.rb
+++ config/cronotab.rb
@@ -13,5 +13,5 @@
 #
 # Crono.perform(TestJob).every 2.days, at: '15:30'
 #
-Crono.perform(TodayServicesReminderJob).every 1.days, at: { hour: 07, min: 00 }
-Crono.perform(TomorrowServicesReminderJob).every 1.days, at: { hour: 20, min: 00 }
+Crono.perform(TodayServicesReminderJob).every 1.minutes#, at: { hour: 07, min: 00 }
+Crono.perform(TomorrowServicesReminderJob).every 1.minutes#, at: { hour: 20, min: 00 }
